2be05b31a8830f8d859dd6817f0c9d80a3bbadfc
Shaina08 <20bee040@nith.ac.in>
2023-07-25 22:26:37 +0530

    Changes

--- src/container/Spline/Spline.jsx
+++ src/container/Spline/Spline.jsx
@@ -5,7 +5,8 @@ import React from 'react'
 
 const Splinetool = () => {
   return (
-    <Spline scene="https://prod.spline.design/N9Yi-qf3Ux0R301X/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/ByrMby3L25nbBwos/scene.splinecode" />
+    // <Spline scene="https://prod.spline.design/Msp6UuA2pgn4ep-H/scene.splinecode" />
   )
 }
 
